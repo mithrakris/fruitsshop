29c3ad66c98ff63e3d2c7ed46d8f83242c99ea21
mithrakris <130356029+mithrakris@users.noreply.github.com>
2023-04-16 10:14:29 +0530

    Add files via upload

---
+++ def_fruitshop.py
@@ -0,0 +1,14 @@
+def fruits(a,b,c):
+    fruitshop=a+b+c
+    print(fruitshop)
+apple=50
+oranges=60
+mango=100
+watermelon=500
+def standardfruit():
+    fruitshop=apple+oranges+mango
+    print(fruitshop)
+
+fruits(watermelon,oranges,mango)
+standardfruit()
+
